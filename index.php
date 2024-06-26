43d03194c2bd43c80aeffa38c7fe54d0a717bff7
Dev Mewada <mewadadev03@gmail.com>
2024-06-26 20:12:55 +0530

    accordian

--- index.php
+++ index.php
@@ -10,20 +10,23 @@ include("database.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Project</title>
     <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="main">
         <div class="search">
             <form action="index.php" method="POST">
-                <div>
-                    <input type="search" name="query">
-                    <input type="number" name="year" min="2000" max="2050" step="1" placeholder="Enter Year">
-                    <button type="submit" name="submit">Search</button>
+                <div class="search-bar">
+                    <input type="search" name="query" placeholder="Title / Keyword">
+                    <input type="number" name="year" min="2000" max="2050" step="1" placeholder="Year">
                 </div>
+                <button type="submit" name="submit">Search</button>
             </form>
         </div>
-        <div class="project-section">
+        <div class="project-section ">
             <?php
             if (isset($_POST["submit"])) {
                 $query = $_POST["query"];
@@ -57,9 +60,12 @@ include("database.php");
 
                     if (mysqli_num_rows($result) > 0) {
                         while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<div class='project'>";
-                            echo "<h3>" . ($row["title"]) . "</h3>";
-                            echo "<p>ID: " . htmlspecialchars($row["pID"]) . "</p>";
+                            echo "<div class='accordian'>";
+                            echo "<div class='accordian-child'>";
+                            $id = $row["pID"];
+                            echo "<input type='radio' name='accordian' id='<?php echo(`$id`); ?>'>";
+                            echo "<label for='<?php echo(`$id`); ?>'>" . ($row["title"]) . "</label>";
+                            echo "<div class='content'>";
                             echo "<p>Students Required: " . htmlspecialchars($row["studentReq"]) . "</p>";
                             echo "<p>Short Description: " . htmlspecialchars($row["sDesc"]) . "</p>";
                             echo "<p>Detailed Description: </p>";
@@ -77,28 +83,21 @@ include("database.php");
                             echo "<p>Application End Date: " . htmlspecialchars($row["appEdate"]) . "</p>";
                             echo "<p>Project Start Date: " . htmlspecialchars($row["pSdate"]) . "</p>";
                             echo "<p>Project End Date: " . htmlspecialchars($row["pEdate"]) . "</p>";
-                            if ($row["status"] == 1) {
-                                // echo "<p>Active</p>";
-                                $status = "Active";
-                            } else {
-                                // echo "<p>Deactive</p>";
-                                $status = "Inactive";
-                            }
+                            if ($row["status"] == 1) $status = "Active";
+                            else $status = "Inactive";
                             echo "<p>Status: " . $status . "</p>";
                             echo "<p>Type: " . htmlspecialchars($row["iType"]) . "</p>";
                             echo "<p>Extra: " . htmlspecialchars($row["extra"]) . "</p>";
                             echo "</div>";
+                            echo "</div>";
+                            echo "</div>";
                         }
-                        echo "</table>";
                     } else {
                         echo "No results found";
                     }
                 }
             }
-
             ?>
-        </div>
-    </div>
 </body>
 
 </html>
